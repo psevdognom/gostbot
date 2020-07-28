b7ffbaab3385525c73fa6d85035695aa33b40100
Kostuxa <samsonov.k.m@gmail.com>
2020-07-28 14:57:44 +0300

    добавлено приветственное слово бота

--- tgbot/bot.py
+++ tgbot/bot.py
@@ -1,10 +1,10 @@
 from telebot import TeleBot
 
-from .settings import API_TOKEN
+from settings import API_TOKEN
 
 bot = TeleBot(API_TOKEN)
 
-@bot.message_handler(commands=['start'])
+@bot.route('/start')
 def start_message(message):
     bot.send_message(message.chat.id, 'Введите номер госта для поиска')
 
