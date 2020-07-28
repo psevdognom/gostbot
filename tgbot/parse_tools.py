d9550fd71e2c6b8fd8ca17e08b302eb63344059c
Kostuxa <samsonov.k.m@gmail.com>
2020-07-29 00:56:10 +0300

    изменение кодировки файла

--- tgbot/parse_tools.py
+++ tgbot/parse_tools.py
@@ -12,6 +12,7 @@ def get_search_list(search_text):
     }
     r = requests.get(SEARCH_URL, params=params)
     search_list = []
+    soup = BeautifulSoup(r.content, 'html')
     return search_list
     # TODO по аналогии с видео с парсингом сайтов достать из страницы ответ r.content или r.text все найденнные госты
     # пока что только на первой странице и записать в search_list
