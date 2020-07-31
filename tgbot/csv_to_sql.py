ea258ed8c4d49e50a89bd7c38dbcc917c96eaf66
Konstantin Ponomarev <konstantinponomarev@protonmail.com>
2020-07-31 19:02:48 +0300

    added sqlalchemy support

--- tgbot/csv_to_sql.py
+++ tgbot/csv_to_sql.py
@@ -1,6 +1,6 @@
 import requests
 from lxml import html
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
